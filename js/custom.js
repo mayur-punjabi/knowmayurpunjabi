e6e9bae77d0d9c97753ab8fa7ef77efc3f2ed02e
mayur7099 <mayurpunjabi9@gmail.com>
2018-04-10 10:29:33 +0530

    initialized repo

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+    var userFeed = new Instafeed({
+        get: 'user',
+        userId: '1302484905',
+        limit: 9,
+        resolution: 'standard_resolution',
+        accessToken: '1302484905.1677ed0.b75d92da797f4879a118d278e6f4a395',
+        sortBy: 'most-recent',
+        template: '<div class="col-lg-4 col-md-6 col-sm-12 instaimg"><a href="{{link}}"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+    });
+
+
+    userFeed.run();
+
+    
+
+
+});
