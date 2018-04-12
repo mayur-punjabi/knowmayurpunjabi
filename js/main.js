ce85651db31da5ab674b92b284c8c715c5c6339a
mayur punjabi <mayurpunjabi9@gmail.com>
2018-04-12 09:19:08 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//var ctype = navigator.connection.effectiveType;
+//console.log(ctype);
+//document.querySelector("#notch").innerText = ctype;
+
+
+
+var connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
+var ctype = connection.type;
+console.log(ctype)
+function updateConnectionStatus() {
+  console.log("Connection type changed from " + ctype + " to " + connection.type);
+}
+
+connection.addEventListener('change', updateConnectionStatus);
+
+
+navigator.getBattery().then(function(battery) {
+       
+    
+
+    console.log(battery.level);
+});
+
+var d = new Date(); // for now
+console.log(d.getHours()); // => 9
+console.log(d.getMinutes()); // =>  30
+d.getSeconds(); // => 51
